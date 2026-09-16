186d5f257b305b0ccc80704cb214febc717cc44c
hamza <hamzanasim248@gmail.com>
2026-09-16 20:45:41 +0500

    Add Microsoft Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             gtag('config', 'G-Z9J49J07HS');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj6kpwnnpm");
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
         <Header />
