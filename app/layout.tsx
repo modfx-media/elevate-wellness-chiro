bebeeaa1a151a13b53bae95104645da7375e335e
Nargis Fatima <nargis@modfxmedia.com>
2026-09-26 00:18:09 +0500

    meta pixel site wide

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,8 +64,31 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             })(window, document, "clarity", "script", "yj6kpwnnpm");
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1626902627923012');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1626902627923012&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Header />
         {children}
         <Footer />
